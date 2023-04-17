c76340aeff4a5e03be0aef67e545c15803fd1e71
moses <cayvoh@mosess-MacBook-Pro.local>
2023-04-18 01:38:33 +0300

    basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$('body').prepend('<p>Holberton Dashboard</p>');
+$('body').prepend('<p>Dashboard data for the students</p>');
+$('body').prepend('<p>Copyright - Holberton School</p>');
