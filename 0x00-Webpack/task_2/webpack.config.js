ac743194ea76f0e02589211b261c1fbe94e23eff
moses <cayvoh@mosess-MacBook-Pro.local>
2023-04-19 09:11:27 +0300

    adding css and images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+             },
+            },
+          ],
+        },
+      ],
+    },
+  };
