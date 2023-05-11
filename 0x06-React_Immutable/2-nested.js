f95b662ed8b16ee34b41ae302be0305029c76d27
Cayvoh-K <muhiakevin12@gmail.com>
2023-05-11 14:00:09 +0000

    nested elements

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+import { fromJS } from "immutable";
+
+export default function accessImmutableObject(object, array) {
+const mappedObj = fromJs(object);
+	
+	return mappedObj.getIn(array, undefined);
+}
